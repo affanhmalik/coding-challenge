cee90b19c0544c671347b4265a7fa1488e6399cd
Affan Malik <affanhmalik@gmail.com>
2018-07-04 13:30:05 -0400

    Changed Person property names to match json keys. Added test cases.

--- java/src/main/java/com/interset/interview/Person.java
+++ java/src/main/java/com/interset/interview/Person.java
@@ -1,31 +1,113 @@
 package com.interset.interview;
 
 import java.time.Instant;
-import java.time.ZoneOffset;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.Date;
-import java.util.TimeZone;
+import java.time.format.DateTimeFormatter;
 
 public class Person {
-	String firstName;
-	String lastName;
-	int siblings;
-	String favouriteFood;
-	String timeZoneOffset;
-	long birthTimestamp;
+	
+	private String first_name;
+	private String last_name;
+	private int siblings;
+	private String favourite_food;
+	private String birth_timezone;
+	private long birth_timestamp;
+
 	
 	public Person(String firstName,String lastName,int siblings,String favouriteFood,String timeZoneOffset,long birthTimestamp) {
-		this.firstName = firstName;
-		this.lastName = lastName;
+
+		this.first_name = firstName;
+		this.last_name = lastName;
 		this.siblings = siblings;
-		this.favouriteFood = favouriteFood;
-		this.timeZoneOffset = timeZoneOffset;
-		this.birthTimestamp = birthTimestamp; 
+		this.favourite_food = favouriteFood;
+		this.birth_timezone = timeZoneOffset;
+		this.birth_timestamp = birthTimestamp;		
 		
 	}
 	
+	public int getBirthMonth() {		
+		ZoneId birthTimeZone = ZoneId.of(this.getBirth_timzone());		
+//		ZoneId birthTimeZone = ZoneId.of("+00:00");
+		Instant birthDayInstant = Instant.ofEpochMilli(this.getBirth_timestamp());
+		ZonedDateTime birthDateTime = ZonedDateTime.ofInstant(birthDayInstant,birthTimeZone);
+		return birthDateTime.getMonthValue();
+	}
+	
+	public String getFirst_name() {
+		return first_name;
+	}
+
+
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+
+
+	public int getSiblings() {
+		return siblings;
+	}
+
+
+
+	public void setSiblings(int siblings) {
+		this.siblings = siblings;
+	}
+
+
+
+	public String getFavourite_food() {
+		return favourite_food;
+	}
+
+
+
+	public void setFavourite_food(String favourite_food) {
+		this.favourite_food = favourite_food;
+	}
+
+
+
+	public String getBirth_timzone() {
+		return birth_timezone;
+	}
+
+
+
+	public void setBirth_timzone(String birth_timzone) {
+		this.birth_timezone = birth_timzone;
+	}
+
+
+
+	public long getBirth_timestamp() {
+		return birth_timestamp;
+	}
+
+
+
+	public void setBirth_timestamp(long birth_timestamp) {
+		this.birth_timestamp = birth_timestamp;
+	}
+
+
+	@Override
 	public String toString() {
 		
-		return "Person Name: " + this.firstName + " " + this.lastName + " , Siblings: " + this.siblings + " Born: " + Instant.ofEpochMilli(this.birthTimestamp) ;
+		return "Person Name: " + this.first_name + " " + this.last_name + " , Siblings: " + this.siblings + " Born: " + Instant.ofEpochMilli(this.birth_timestamp) + ", Timezone: " + this.birth_timezone + ", Favorite Food: " + this.favourite_food ;
 	}
 }
--- java/src/test/java/com/interset/interview/PersonTest.java
+++ java/src/test/java/com/interset/interview/PersonTest.java
@@ -3,13 +3,18 @@ package com.interset.interview;
 import org.junit.*;
 import static org.junit.Assert.*;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Unit test for Runner
  */
 public class PersonTest 
 {
 
-    // Example test
+    // Test Person objects
     @Test
     public void createPersonObject()
     {     
@@ -17,9 +22,33 @@ public class PersonTest
         Person newPerson2 = new Person("EUGENE","VANDERSTEEN",2,"Yogurt","+01:00",853371780000L);
     		Person newPerson3 = new Person("BERNARDINA","STWART",1,"Mozzarella cheese","+10:30",285926100000L);
     		
+    		
     		assertTrue( newPerson1 instanceof Person);
     		assertTrue( newPerson2 instanceof Person);
     		assertTrue( newPerson3 instanceof Person);
+    		assertEquals(newPerson1.getLast_name(),"MCCRAE");
+    		assertEquals(newPerson2.getLast_name(),"VANDERSTEEN");
+    		assertEquals(newPerson3.getLast_name(),"STWART");
+    }
+    
+    @Test
+    public void testBirthMonthFunction() {
+	    	Person testPerson1 = new Person("Joe","One",3,"Chocolate","+03:00",760057996000L); // Monday, January 31, 1994 11:13:16 PM (UTC)
+	    	Person testPerson2 = new Person("Joe","Two",1,"Chocolate","-05:00",560045596000L); // Thursday, October 1, 1987 12:13:16 AM (UTC)
+	    	Person testPerson3 = new Person("Joe","Three",5,"Chocolate","+07:30",924264796000L); // Friday, April 16, 1999 12:13:16 PM (UTC)  
+	    	Person testPerson4 = new Person("Joe","Four",0,"Chocolate","+10:00",383874301000L); // Wednesday, February 29, 1984 11:45:01 PM  
+	    	
+	    	testPersonBirthMonth(testPerson1, 2);
+	    	testPersonBirthMonth(testPerson2, 9);
+	    	testPersonBirthMonth(testPerson3, 4);
+	    	testPersonBirthMonth(testPerson4, 3);
+    	
+    }
+    
+    // 
+    public void testPersonBirthMonth(Person testPerson, int expectedBirthMonth)
+    {
+    		assertEquals(testPerson.getBirthMonth(),expectedBirthMonth);
     }
 }
 
